39fff6bd6467f1b13a69082f3de77b66ce3720ce
Jean Molossi <jean.carlosmolossi@gmail.com>
2023-06-27 01:15:18 -0300

    ci: fix storybook version

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,28 +1,26 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: [
-	"../src/pages/**/*.mdx",
-	"../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-  async viteFinal(config, { configType }) {
-	if (configType === 'PRODUCTION') {
-		config.base = '/szr-ui/'
-	}
+module.exports = {
+	"stories": [
+		"../src/pages/**/*.stories.mdx",
+		"../src/stories/**/*.stories.tsx"
+	],
+	"addons": [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+		"@storybook/addon-a11y"
+	],
+	"framework": "@storybook/react",
+	"core": {
+		"builder": "@storybook/builder-vite"
+	},
+	"features": {
+		"storyStoreV7": true
+	},
+	viteFinal: (config, { configType }) => {
+		if (configType === 'PRODUCTION') {
+			config.base = '/szr-ui/'
+		}
 
-	return config
-  },
+		return config
+	},
 };
-export default config;
--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,24 +1,14 @@
 import { themes } from '@storybook/theming'
-import bgs from '@storybook/addon-backgrounds'
 
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-	docs: {
-		theme: themes.dark
+export const parameters = {
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
 	},
-	backgrounds: {
-		default: 'dark',
-		theme: themes.dark
+	docs: {
+		theme: themes.dark,
 	}
-  },
-};
-
-export default preview;
+}
